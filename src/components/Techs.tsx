10ad74909efc2932a79ba1a2dfab3afc33848ff3
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-17 08:56:50 +0900

    rm: remove description in mobile

--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -8,7 +8,10 @@ export default function Techs() {
         <div className='flex flex-row gap-3 flex-wrap'>
           <LanguageBox name='TypeScript' logoUrl='/logos/ts.svg' description='가장 많이 사용하며, 웹에 주로 사용합니다.' />
           <LanguageBox name='JavaScript' logoUrl='/logos/js.svg' description='간단한 프로그램에 사용합니다.' />
+          <LanguageBox name='Next.js' logoUrl='/logos/nextjs.svg' description='TypeScript와 함께 웹에 주로 사용합니다.' />
           <LanguageBox name='Java' logoUrl='/logos/java.svg' description='주로 마인크래프트 플러그인을 만들 때 사용합니다.' />
+          <LanguageBox name='PostgresSQL' logoUrl='/logos/psql.svg' description='보통 웹 프로젝트에 사용합니다.' />
+          <LanguageBox name='Python' logoUrl='/logos/py.svg' description='알고리즘이나 간단한 프로그램에 사용합니다.' />
         </div>
       </div>
     </div>

205322976fff599b3c75efb8542553a0bb0fe90c
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-17 06:43:08 +0900

    feat: support mobile

--- src/components/LanguageBox.tsx
+++ src/components/LanguageBox.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 export default function LanguageBox({ logoUrl, name, description }: Props) {
   return (
-    <div className='h-32 w-64 rounded-md bg-slate-200 text-base flex'>
+    <div className='inline h-32 w-64 min-w-[16rem] rounded-md bg-slate-200 text-base flex'>
       <img className='m-[14px]' src={logoUrl} alt={name} height={100} width={100} />
       <div className='my-[14px]'>
         <p className='font-bold'>{name}</p>
-        <p className='font-normal break-all'>
+        <p className='text-sm font-normal break-all'>
           {description}
         </p>
       </div>

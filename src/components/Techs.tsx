00b4073c096153d7ca69d78948e7274b469101bf
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-17 11:39:50 +0900

    feat: center in mobile

--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -3,9 +3,9 @@ import LanguageBox from './LanguageBox'
 export default function Techs() {
   return (
     <div className='snap-start w-screen h-screen'>
-      <div className='p-24 sm:text-4xl text-2xl font-light'>
+      <div className='text-center sm:text-left p-24 sm:text-4xl text-2xl font-light'>
         <p className='mb-2'>저는 이러한 기술들을 사용할 수 있습니다.</p> 
-        <div className='flex flex-row gap-3 flex-wrap xl:w-[calc((24rem+12px)*3)]'>
+        <div className='flex flex-row max-sm:justify-center gap-3 flex-wrap xl:w-[calc((24rem+12px)*3)]'>
           <LanguageBox name='TypeScript' logoUrl='/logos/ts.svg' description='가장 많이 사용하며, 웹에 주로 사용합니다.' />
           <LanguageBox name='JavaScript' logoUrl='/logos/js.svg' description='간단한 프로그램에 사용합니다.' />
           <LanguageBox name='Next.js' logoUrl='/logos/nextjs.svg' description='TypeScript와 함께 웹에 주로 사용합니다.' />

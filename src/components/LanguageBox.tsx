f87e4a3c583c987d9ca0616ab4b8b22896d9bdd3
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-17 11:10:29 +0900

    fix: asdpofijqopwjfvscwer

--- src/components/LanguageBox.tsx
+++ src/components/LanguageBox.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default function LanguageBox({ logoUrl, name, description }: Props) {
   return (
-    <div className='max-md:inline md:h-32 md:w-64 rounded-md md:bg-slate-200 text-base flex'>
-      <img className='md:m-[14px]' src={logoUrl} alt={name} height={100} width={100} />
-      <div className='max-md:hidden my-[14px]'>
+    <div className='max-lg:inline lg:h-32 lg:w-96 rounded-lg lg:bg-slate-200 text-base flex'>
+      <img className='lg:m-[14px]' src={logoUrl} alt={name} height={100} width={100} />
+      <div className='max-lg:hidden my-[14px]'>
         <p className='font-bold'>{name}</p>
         <p className='text-sm font-normal break-all'>
           {description}

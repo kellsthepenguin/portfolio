b2926d95f5fecc45349937ef8bd867a9aa1534a3
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-02-18 08:20:34 +0900

    change colors of boxes

--- src/components/CareerBox.tsx
+++ src/components/CareerBox.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function CareerBox({ name, logoUrl, from, to, description }: Props) {
   return (
-    <div className='max-md:snap-start h-96 w-96 min-w-[24rem] bg-slate-200 rounded-md text-base shrink-0 flex flex-col items-center'>
+    <div className='max-md:snap-start h-96 w-96 min-w-[24rem] bg-gray-50 rounded-md text-base shrink-0 flex flex-col items-center'>
       <p className='mt-2 font-bold'>{name}</p>
       <img src={logoUrl} width={256} height={256} />
       <p>{from} ~ {to}</p>
--- src/components/LanguageBox.tsx
+++ src/components/LanguageBox.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function LanguageBox({ logoUrl, name, description }: Props) {
   return (
-    <div className='max-lg:inline lg:h-32 lg:w-96 rounded-lg lg:bg-slate-200 text-base flex'>
+    <div className='max-lg:inline lg:h-32 lg:w-96 rounded-lg lg:bg-gray-50 text-base flex'>
       <img className='lg:m-[14px]' src={logoUrl} alt={name} height={100} width={100} />
       <div className='max-lg:hidden my-[14px]'>
         <p className='font-bold'>{name}</p>
